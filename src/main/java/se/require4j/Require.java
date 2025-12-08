d043306603cfc5b502e7cf511dff5f0ab3bc4344
julleb <julleb@julleb.se>
2025-12-08 20:08:49 +0100

    private constructor for Require

--- src/main/java/se/require4j/Require.java
+++ src/main/java/se/require4j/Require.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 
 public class Require {
 
+    private Require() {}
 
     public static void NonNull(Object obj, Supplier<RuntimeException> exceptionSupplier) {
         if(obj == null) {
